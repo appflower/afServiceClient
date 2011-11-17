c941b3dff4b25fd9ba617e36572f156f70e7927a
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2011-11-17 14:54:16 +0100

    changed parameters for CreateProject

--- src/lib/afServiceClient.php
+++ src/lib/afServiceClient.php
@@ -2,6 +2,7 @@
 /**
  * This class provides gateway to api services provided by appFlowerService project
  *
+ * @todo - remove afRestWSPlugin requirement - we should use some symfony2 components
  * @author lukas
  */
 class afServiceClient
@@ -22,50 +23,36 @@ class afServiceClient
      */
     static function create($url)
     {
-//        $domain = sfConfig::get('app_seedcontrol_url');
-//        $url = 'http://'.$domain.'/api.php';
-
         if ($url == '') {
-            throw new Exception('You must provide some URL');
+            throw new afServiceException('You must provide some URL');
         }
         $client = new afRESTWSClient();
         $client->setBaseUrl($url);
         
-//        $logger = sfContext::getInstance()->getLogger();
-//        $client->setLogger($logger);
-
-        return new self($client);
+        return new afServiceClient($client);
     }
 
-    function CreateProject($slug, $email, $firstName, $lastName)
+    /**
+     * @return afRESTWSResponse
+     */
+    function CreateProject($projectName, $email, $adminPersonName)
     {
+        /**
+         * @todo - slug should be builded by appFlowerService
+         */
         $parameters = array(
             'payload' => json_encode(array(
-                'slug' => $slug,
-                'name' => $slug,
+                'name' => $projectName,
+                'slug' => $projectName,
                 'user' => array(
-                    'first_name' => $firstName,
-                    'last_name'  => $lastName,
-                    'email'      => $email
+                    'name' => $adminPersonName,
+                    'email'=> $email
                 )
             ))
         );
         $request  = $this->client->createRequest($parameters, "project/create", 'POST');
         $response = $this->client->send($request);
-
-        if ($response->isSuccess()) {
-            return $response->getMessage();
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * @return afRESTWSClient
-     */
-    function getClient()
-    {
-        return $this->client;
+        return $response;
     }
 }
 ?>
